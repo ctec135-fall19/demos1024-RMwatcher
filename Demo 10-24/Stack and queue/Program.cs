ac15207ba69b3e6184fe3e69162cd65cc0526ac8
Richard <r.mora@students.clark.edu>
2019-10-24 14:53:41 -0700

    Demo 10/24 complete

---
+++ Demo 10-24/Stack and queue/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_and_queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Stacks
+            // declaration
+            Stack<student> stackOfStudents = new Stack<student>();
+
+            // push objects onto stack
+            stackOfStudents.Push(new student("Bill"));
+            stackOfStudents.Push(new student("JIll"));
+            stackOfStudents.Push(new student("Jack"));
+
+            // traversal
+            foreach (student s in stackOfStudents)
+            {
+                Console.WriteLine(s.Name);
+            }
+            Console.WriteLine(stackOfStudents.Count);
+            Console.WriteLine();
+
+            // pops
+            int size = stackOfStudents.Count;
+            for (int i = 0; i < size; i++) {
+                Console.WriteLine(stackOfStudents.Pop().Name);
+            }
+            Console.WriteLine(stackOfStudents.Count);
+            Console.WriteLine();
+
+            // queue
+            Queue<student> studentQueue = new Queue<student>();
+
+            // put items in the queue
+            studentQueue.Enqueue(new student("Bill"));
+            studentQueue.Enqueue(new student("Jill"));
+            studentQueue.Enqueue(new student("Jack"));
+
+            // traversal
+            foreach (student s in studentQueue)
+            {
+                Console.WriteLine(s.Name);
+            }
+            Console.WriteLine(studentQueue.Count);
+            Console.WriteLine();
+
+            size = studentQueue.Count;
+            for (int i = 0; i < size; i++)
+            {
+                Console.WriteLine(studentQueue.Dequeue().Name);
+            }
+            Console.WriteLine(studentQueue.Count);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Demo 10-24/Stack and queue/student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_and_queue
+{
+    class student
+    {
+        public string Name { get; set; }
+
+        public student(string name) { name = Name; }
+    }
+}
